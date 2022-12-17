fc7ad2d51cf56d8ba4a79bb52c82778a77597b3b
Gaudry Carlier <gaudry.carlier@utt.fr>
2022-12-17 22:33:53 +0100

    Fix: delete some unused files et format document

--- c/main.c
+++ c/main.c
@@ -1,41 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct
+{
     char *name;
     int poids;
     int val;
     int nombre;
-} item_t;  // Structure d'un item
+} item_t; // Structure d'un item
 
-
-
-int *knapsack (int w, int n, item_t* items) { // On réalise le théroème du sac à dos
-    int i, j, k, v, *mm, **m, *s; // On déclare les variables
-    mm = calloc((n + 1) * (w + 1), sizeof (int)); // On alloue la mémoire
-    m = malloc((n + 1) * sizeof (int *));
+int *knapsack(int w, int n, item_t *items)
+{                                                // On réalise le théroème du sac à dos
+    int i, j, k, v, *mm, **m, *s;                // On déclare les variables
+    mm = calloc((n + 1) * (w + 1), sizeof(int)); // On alloue la mémoire
+    m = malloc((n + 1) * sizeof(int *));
     m[0] = mm;
-    for (i = 1; i <= n; i++) {  // On parcourt la liste d'items
+    for (i = 1; i <= n; i++)
+    { // On parcourt la liste d'items
         m[i] = &mm[i * (w + 1)];
-        for (j = 0; j <= w; j++) {
+        for (j = 0; j <= w; j++)
+        {
             m[i][j] = m[i - 1][j];
-            for (k = 1; k <= items[i - 1].nombre; k++) {
-                if (k * items[i - 1].poids > j) { // Si le poids de l'item est supérieur au poids du sac
+            for (k = 1; k <= items[i - 1].nombre; k++)
+            {
+                if (k * items[i - 1].poids > j)
+                { // Si le poids de l'item est supérieur au poids du sac
                     break;
                 }
                 v = m[i - 1][j - k * items[i - 1].poids] + k * items[i - 1].val; // On calcule la valeur de l'item avec le tri dynamique
-                if (v > m[i][j]) {
+                if (v > m[i][j])
+                {
                     m[i][j] = v; // On met à jour la valeur de l'item
                 }
             }
         }
     }
-    s = calloc(n, sizeof (int));
-    for (i = n, j = w; i > 0; i--) {
+    s = calloc(n, sizeof(int));
+    for (i = n, j = w; i > 0; i--)
+    {
         int v = m[i][j];
         int quantity = items[i - 1].nombre;
-        for (k = 0; v != m[i - 1][j] + k * items[i - 1].val; k++) {
-            if (quantity != 0) { // Si la de l'item est nul, on passe à l'item suivant
+        for (k = 0; v != m[i - 1][j] + k * items[i - 1].val; k++)
+        {
+            if (quantity != 0)
+            { // Si la de l'item est nul, on passe à l'item suivant
                 s[i - 1]++;
                 j -= items[i - 1].poids; // On met à jour le poids du sac
                 quantity--;
@@ -43,37 +51,39 @@ int *knapsack (int w, int n, item_t* items) { // On réalise le théroème du sa
         }
     }
     free(mm);
-    free(m);  // On libère la mémoire
+    free(m); // On libère la mémoire
     return s;
 }
 
 int number_arr = 0;
 int quantite_update[500];
-int main (int C, int number,int *name_item,int *poids_item_list,int *val_item,int *quantity_item) {    // On récupère les données venant de python
+int main(int C, int number, int *name_item, int *poids_item_list, int *val_item, int *quantity_item)
+{                                                                         // On récupère les données venant de python
     printf("Veuillez indiquer le nom du fichier (.txt) de sauvegarde: "); // On demande le nom du fichier de sauvegarde
     char nom_fichier[100];
     scanf("%s", nom_fichier);
-    FILE *fichier = fopen(nom_fichier, "w"); // On ouvre le fichier de sauvegarde
-    fprintf(fichier, "Liste de produits partant: \n");// On écrit dans le fichier de sauvegarde
+    FILE *fichier = fopen(nom_fichier, "w");           // On ouvre le fichier de sauvegarde
+    fprintf(fichier, "Liste de produits partant: \n"); // On écrit dans le fichier de sauvegarde
     char UID[100];
     int n;
     number_arr = number;
     item_t items[number];
     printf("\n\n\nVoici les informations suivantes:   \n");
     printf(" Poids maximum du camion: %d\n", C);
-    printf(" Nombre d'items de la commande: %d\n", number);                         // On affiche les informations dans le terminal et dans le fichier de sauvegarde
+    printf(" Nombre d'items de la commande: %d\n", number); // On affiche les informations dans le terminal et dans le fichier de sauvegarde
     fprintf(fichier, "Poids maximum du camion: %d\n", C);
     fprintf(fichier, "Nombre d'items de la commande: %d\n\n", number);
     printf("|----------------------------------------|\n");
     printf("|                 Resume                 |\n");
     printf("|UID produit:    quantite: poids: valeur:|\n");
     printf("|----------------------------------------|\n");
-    for (int i=0;i<number;i++){
-        sprintf(UID, "%d", name_item[i] );
-        items[i].name= UID;
-        items[i].poids=poids_item_list[i];  
-        items[i].val=val_item[i];
-        items[i].nombre=quantity_item[i];
+    for (int i = 0; i < number; i++)
+    {
+        sprintf(UID, "%d", name_item[i]);
+        items[i].name = UID;
+        items[i].poids = poids_item_list[i];
+        items[i].val = val_item[i];
+        items[i].nombre = quantity_item[i];
         printf("|%-22d %5d %5d %5d|\n", name_item[i], items[i].nombre, items[i].poids, items[i].val);
     }
     printf("|----------------------------------------|\n\n\n");
@@ -88,11 +98,13 @@ int main (int C, int number,int *name_item,int *poids_item_list,int *val_item,in
     n = number;
     int *s;
     s = knapsack(C, n, items); // On réalise le théorème du sac à dos
-    int i,nb_item = 0, tc = 0, tw = 0, tv = 0;
-    for (i = 0; i < n; i++) {
+    int i, nb_item = 0, tc = 0, tw = 0, tv = 0;
+    for (i = 0; i < n; i++)
+    {
         int condition = 0;
-        if (s[i]) {
-            printf("|%-22d %5d %5d %5d|\n", name_item[i], s[i], s[i] * items[i].poids, s[i] * items[i].val); // On affiche les items choisis
+        if (s[i])
+        {
+            printf("|%-22d %5d %5d %5d|\n", name_item[i], s[i], s[i] * items[i].poids, s[i] * items[i].val);           // On affiche les items choisis
             fprintf(fichier, "|%-22d %5d %5d %5d|\n", name_item[i], s[i], s[i] * items[i].poids, s[i] * items[i].val); // On affiche les items choisis
             tc += s[i];
             tw += s[i] * items[i].poids;
@@ -100,36 +112,41 @@ int main (int C, int number,int *name_item,int *poids_item_list,int *val_item,in
             condition = 1;
             nb_item++;
         }
-        if (condition == 1){
+        if (condition == 1)
+        {
             quantite_update[i] = items[i].nombre - s[i];
         }
-        else{
+        else
+        {
             quantite_update[i] = items[i].nombre;
         }
     }
-    
+
     printf("|----------------------------------------|\n");
     printf("|%-22s %5d %5d %5d|\n", "Total:", tc, tw, tv); // On affiche le nombre, le poids et la valeur totale
     printf("|----------------------------------------|\n\n\n");
     fprintf(fichier, "|----------------------------------------|\n");
     fprintf(fichier, "|%-22s %5d %5d %5d|\n", "Total:", tc, tw, tv); // On affiche le nombre, le poids et la valeur totale
     fprintf(fichier, "|----------------------------------------|\n\n");
-    printf("Nombre d'items differents partant: %d\n\n", nb_item); // On affiche le nombre d'items partant
+    printf("Nombre d'items differents partant: %d\n\n", nb_item);         // On affiche le nombre d'items partant
     fprintf(fichier, "Nombre d'items differents partant: %d\n", nb_item); // On affiche le nombre d'items partant
     free(s);
     fclose(fichier);
     return 0;
 }
 
-int* Getaray(){
+int *Getaray()
+{
     int *arr = malloc(number_arr * sizeof(int));
 
-    for (int i=0;i<number_arr;i++){
+    for (int i = 0; i < number_arr; i++)
+    {
         arr[i] = quantite_update[i];
     }
     return arr;
 }
 
-void free_array(int *arr){
+void free_array(int *arr)
+{
     free(arr);
 }
--- python/test.py
+++ python/test.py
@@ -1,6 +1,8 @@
 from ctypes import alignment, c_int, cdll, POINTER, Structure
 from pathlib import Path
-def open_dll(name= 'test.dll' ):
+
+
+def open_dll(name='test.dll'):
     dll_path = Path(__file__).parent / name
     return cdll.LoadLibrary(str(dll_path))
 
@@ -13,10 +15,10 @@ Quantity = []
 
 print("Veuillez indiquer le nombre de produits dans la commande :")
 nb = int(input())
-Number = c_int(nb) #On convertit le nombre de produits en entier C
+Number = c_int(nb)  # On convertit le nombre de produits en entier C
 print("Veuillez indiquer le poids maximum de la commande :")
 C = int(input())
-C = c_int(C) #On convertit le poids maximum en entier C
+C = c_int(C)  # On convertit le poids maximum en entier C
 for i in range(nb):
     print("Veuillez indiquer la valeur UID du produit n°", i+1, " :")
     uid = int(input())
@@ -26,27 +28,29 @@ for i in range(nb):
     quan = int(input())
     print("Veuillez indiquer le poids du produit n°", i+1, " :")
     poids = int(input())
-    #On rempli les listes avec les valeurs entrées par l'utilisateur
+    # On rempli les listes avec les valeurs entrées par l'utilisateur
     Values.append(uid)
-    Weights.append(poids)              
+    Weights.append(poids)
     Price.append(prix)
     Quantity.append(quan)
 
 Values = (c_int * len(Values))(*Values)
-Weights = (c_int * len(Weights))(*Weights)     #On convertit les listes en tableaux C
-Price = (c_int * len(Price))(*Price)  
-Quantity = (c_int * len(Quantity))(*Quantity)  
+# On convertit les listes en tableaux C
+Weights = (c_int * len(Weights))(*Weights)
+Price = (c_int * len(Price))(*Price)
+Quantity = (c_int * len(Quantity))(*Quantity)
 
 
+lib.main(C, Number, Values, Weights, Price, Quantity, len(
+    Values))  # On récupère le résultat de la fonction
 
-lib.main(C, Number, Values, Weights, Price, Quantity, len(Values)) #On récupère le résultat de la fonction
-
-lib.Getaray.restype = POINTER(c_int) #On récupère le tableau de résultat
+lib.Getaray.restype = POINTER(c_int)  # On récupère le tableau de résultat
 result = lib.Getaray()
 print("|----------------------------------------|")
 print("|  La liste de produits mise à jour est :|")
 print("|UID produit:    quantite: poids: valeur:|")
 for i in range(nb):
-    print('|%-22s' % Values[i], '%5d'% result[i],'%5d'% Weights[i],'%5d|'% Price[i])
+    print('|%-22s' % Values[i], '%5d' %
+          result[i], '%5d' % Weights[i], '%5d|' % Price[i])
 
-lib.free_array(result) #On libère le tableau de résultat
+lib.free_array(result)  # On libère le tableau de résultat

a68b95aef0d0ba331230e72e95d527f4d440e108
Gaudry Carlier <gaudry.carlier@utt.fr>
2022-12-17 18:07:22 +0100

    finished the knapsack problem with C and Ctypes

--- c/main.c
+++ c/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct {
     char *name;
     int poids;
@@ -9,38 +8,7 @@ typedef struct {
     int nombre;
 } item_t;  // Structure d'un item
 
-
-
-// item_t items[] = {                                              // Liste d'items pour le théorème du sac à dos*
-    // {"map",                      9,   150,   1},                // De formes ("Nom", Poids, valeur, nombre (= quantité))
-    // {"compass",                 13,    35,   1},
-    // {"water",                  153,   200,   2},
-    // {"sandwich",                50,    60,   2},
-    // {"glucose",                 15,    60,   2},
-    // {"tin",                     68,    45,   3},
-    // {"banana",                  27,    60,   3},
-    // {"apple",                   39,    40,   3},
-    // {"cheese",                  23,    30,   1},
-    // {"beer",                    52,    10,   3},
-    // {"suntan cream",            11,    70,   1},
-    // {"camera",                  32,    30,   1},
-    // {"T-shirt",                 24,    15,   2},
-    // {"trousers",                48,    10,   2},
-    // {"umbrella",                73,    40,   1},
-    // {"waterproof trousers",     42,    70,   1},
-    // {"waterproof overclothes",  43,    75,   1},
-    // {"note-case",               22,    80,   1},
-    // {"sunglasses",               7,    20,   1},
-    // {"towel",                   18,    12,   2},
-    // {"socks",                    4,    50,   1},
-    // {"book",                    30,    10,   2},
-// };
-
-
-
-int *knapsack (int w, item_t* items) { // On réalise le théroème du sac à dos
-    
-    int n =2;
+int *knapsack (int w, int n, item_t* items) { // On réalise le théroème du sac à dos
     int i, j, k, v, *mm, **m, *s; // On déclare les variables
     mm = calloc((n + 1) * (w + 1), sizeof (int));
     m = malloc((n + 1) * sizeof (int *));
@@ -63,9 +31,13 @@ int *knapsack (int w, item_t* items) { // On réalise le théroème du sac à do
     s = calloc(n, sizeof (int));
     for (i = n, j = w; i > 0; i--) {
         int v = m[i][j];
+        int quantity = items[i - 1].nombre;
         for (k = 0; v != m[i - 1][j] + k * items[i - 1].val; k++) {
-            s[i - 1]++;
-            j -= items[i - 1].poids; // On met à jour le poids du sac
+            if (quantity != 0) {
+                s[i - 1]++;
+                j -= items[i - 1].poids; // On met à jour le poids du sac
+                quantity--;
+            }
         }
     }
     free(mm);
@@ -74,37 +46,31 @@ int *knapsack (int w, item_t* items) { // On réalise le théroème du sac à do
 }
 
 
-int main (int number,int* name_item[number],int* poids_item_list[number],int* val_item[number],int* nombre_item[number]) {
+int main (int C, int number,int *name_item,int *poids_item_list,int *val_item,int *quantity_item) {    // On récupère les données venant de python
     char UID[100];
-    printf("Test\n");
-    //On transforme les arguments en liste d'items
-    // printf("%d nombre \n ",number);
-    printf("%d nom \n ",name_item[0]);
-     printf("%d val \n ",name_item[1]);
-    // printf("%d poids \n ",poids_item_list[0]);
+    int n;
     item_t items[number];
     for (int i=0;i<number;i++){
         sprintf(UID, "%d", name_item[i] );
         items[i].name= UID;
         items[i].poids=poids_item_list[i];  
         items[i].val=val_item[i];
-        items[i].nombre=nombre_item[i];
-        printf("%s nom \n ",items[i].name);
+        items[i].nombre=quantity_item[i];
+        printf("%-22d %5d %5d %5d\n", name_item[i], items[i].nombre, items[i].poids, items[i].val);
     }
-
-    int C = 400;
-    int n = 3;
+    n = number;
     int *s;
-    s = knapsack(C, items); // On réalise le théorème du sac à dos
+    s = knapsack(C, n, items); // On réalise le théorème du sac à dos
     int i, tc = 0, tw = 0, tv = 0;
     for (i = 0; i < n; i++) {
         if (s[i]) {
-            printf("%-22s %5d %5d %5d\n", items[i].name, s[i], s[i] * items[i].poids, s[i] * items[i].val); // On affiche les items choisis
+            printf("%-22d %5d %5d %5d\n", name_item[i], s[i], s[i] * items[i].poids, s[i] * items[i].val); // On affiche les items choisis
             tc += s[i];
             tw += s[i] * items[i].poids;
             tv += s[i] * items[i].val;
         }
     }
     printf("%-22s %5d %5d %5d\n", "nombre, poids, val:", tc, tw, tv); // On affiche le nombre, le poids et la valeur totale
+    free(s);
     return 0;
 }
--- python/test.py
+++ python/test.py
@@ -1,30 +1,41 @@
-from ctypes import *
+from ctypes import alignment, c_int, cdll
 from pathlib import Path
-print("Test\n")
 def open_dll(name= 'test.dll' ):
     dll_path = Path(__file__).parent / name
     return cdll.LoadLibrary(str(dll_path))
 
-print("Test\n")
 
-list_nom = (c_int * 20)()
-list_prix = (c_int * 3)()
-list_poids = (c_int * 3)()
-list_quantite = (c_int * 3)()
+lib = open_dll()
+Values = []
+Weights = []
+Price = []
+Quantity = []
+
+print("Veuillez indiquer le nombre de produits dans la commande :")
+nb = int(input())
+Number = c_int(nb) #On convertit le nombre de produits en entier C
+print("Veuillez indiquer le poids maximum de la commande :")
+C = int(input())
+C = c_int(C) #On convertit le poids maximum en entier C
+for i in range(nb):
+    print("Veuillez indiquer la valeur UID du produit n°", i+1, " :")
+    uid = int(input())
+    print("Veuillez indiquer le prix du produit n°", i+1, " :")
+    prix = int(input())
+    print("Veuillez indiquer la quantité du produit n°", i+1, " :")
+    quan = int(input())
+    print("Veuillez indiquer le poids du produit n°", i+1, " :")
+    poids = int(input())
+    #On rempli les listes avec les valeurs entrées par l'utilisateur
+    Values.append(uid)
+    Weights.append(poids)              
+    Price.append(prix)
+    Quantity.append(quan)
+
+Values = (c_int * len(Values))(*Values)
+Weights = (c_int * len(Weights))(*Weights)     #On convertit les listes en tableaux C
+Price = (c_int * len(Price))(*Price)  
+Quantity = (c_int * len(Quantity))(*Quantity)  
     
 
-if __name__ ==  '__main__' :
-    dll = open_dll()
-    print("combien de produits voulez vous ajouter ?")
-    count = 2
-    list_nom[0] =  90856
-    list_prix[0] = 1
-    list_poids[0] = 3
-    list_quantite[0] = 2
-    list_nom[1] =  98634
-    list_prix[1] = 2
-    list_poids[1] = 4
-    list_quantite[1] = 3
-    print("Test")
-    
-    dll.main(count, pointer(list_nom), pointer(list_prix), pointer(list_poids), pointer(list_quantite))
+lib.main(C, Number, Values, Weights, Price, Quantity, len(Values)) #On récupère le résultat de la fonction
